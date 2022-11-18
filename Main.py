46a5630a2bb070262ab5527ef194a30f84242d76
kiril <ender-fox@mail.ru>
2022-11-18 21:58:10 +0300

    Рабочий алгоритм с условиями.

--- Main.py
+++ Main.py
@@ -40,6 +40,43 @@ class minify_js():
             i += 1     # следующий номер
 
 
+    def cond_par(self,m):
+        if m[0][-1]==' ':m[0]=m[0][:-1]
+        for i in range(1,len(m)):
+            if m[i]=='': continue
+            if m[i]==' ': continue
+            if not re.search(r"[+*/<>=-]{1,2}", m[i]):
+                if m[i] in self.nm.keys(): m[i] = self.nm[m[i]];
+                continue
+            vz = re.findall(r"[<>=]{1,2}", m[i])[0]
+            vr=re.split(r" [<>=]{1,2} ", m[i])
+            if vr[0] in self.nm.keys(): vr[0]=self.nm[vr[0]]
+            if vr[1] in self.nm.keys(): vr[1]=self.nm[vr[1]]
+            elif "\'" in m[i] or "\"" in m[i]:  # если строки
+                sa = re.split(r' \+ ', m[i])  # разбираем на части
+                for j in range(len(sa)):  # перебираем ищя переменные
+                    if sa[j] in self.nm.keys(): sa[j] = self.nm[sa[j]]  # заменяем переменные
+                m[i] = ''
+                for j in range(len(sa) - 1):
+                    m[i] += sa[j] + '+'  # соединяем
+                m[i] += sa[-1]
+                continue  # идем к следующему аргументу
+            else:
+                vr[1] = re.sub(r' ', '', vr[1])  # убираем лишние пробелы
+                sz=re.findall(r"[+*/\(\)-]{1,2}", vr[1])
+                sl=re.split(r"[+*/\(\)-]{1,2}", vr[1])
+                f=1
+                for j in range(len(sl)):
+                    if sl[j] in self.nm.keys(): vr[0]=self.nm[sl[j]]; f=0
+                vr[1]=''
+                for j in range(len(sz)):
+                    vr[1] += sl[j]+sz[j]
+                vr[1] += sl[-1]
+                if f: vr[1]=str(eval(vr[1]))
+            m[i]=vr[0]+vz+vr[1]
+        return m
+
+
     def func_par(self,m):
         if ' ' in m[0] and m[0][-1]!=' ':     # если объявление функции
             im=m[0].split(' ')     # разбираем
@@ -49,7 +86,7 @@ class minify_js():
             m[0]=self.nm[m[0].replace(' ','')]     # заменяем имя
 
         for i in range(1,len(m)):     # идем по аргументам
-            if m[i]=='' or m[i]==' ': continue     # пропускаем пустые строки
+            if m[i]=='' or m[i]==' ': m[i]=''; continue     # пропускаем пустые строки
             if "\'"in m[i] or "\"" in m[i]:     # если строки
                 sa=re.split(r' \+ ', m[i])     # разбираем на части
                 for j in range(len(sa)):     # перебираем ищя переменные
@@ -74,13 +111,19 @@ class minify_js():
         out = re.sub(r'[\t\n;]', '', out)     # убираем лишние символы
 
         if re.search(r"\(.*\)",out):     # усли связано с функцией
-            sp=re.split(r"[\(\)\[\]\{\},\.]",out)     # разбиваем на составляющие
-            zn=re.findall(r"[\(\)\[\]\{\},\.]",out)     # запоминаем по чему разделяли
-
-            sp=self.func_par(sp)     # обрабатываем
+            out=re.sub(r', ',',',out)
+            if re.search(r"(?:if|else if|while|for)",out):
+                sp = re.split(r"(?:\(|\)|and|or)",out)     # разбиваем на составляющие
+                zn = re.findall(r"(?:\(|\)|and|or)", out)  # запоминаем по чему разделяли
+                sp=self.cond_par(sp)
+            else:
+                sp=re.split(r"[\(\)\[\]\{\},\.]",out)     # разбиваем на составляющие
+                zn=re.findall(r"[\(\)\[\]\{\},\.]",out)     # запоминаем по чему разделяли
+                sp=self.func_par(sp)     # обрабатываем
             out=''
             for i in range(len(zn)):
                 out+=sp[i]+zn[i]     # собираем
+            if sp[-1]==' {': sp[-1]='{'
             out+=sp[-1]
 
         elif re.search(r"[+*/<>=-]{1,2}",out):     # если переменная
@@ -116,9 +159,9 @@ class minify_js():
             if self.t==1 and len(out) > 0 and out[-1] not in ['(', ',', '{', '}', ';']: out += ';'      # если нужно добавляем
         print(out)
         f.close()   # закрываем файл с иходным кодом
-        # fl = open(self.out_file, 'w')     # открываем выходной файл
-        # fl.write(out)     # записываем новый код
-        # fl.close()   # закрываем файл
+        fl = open(self.out_file, 'w')     # открываем выходной файл
+        fl.write(out)     # записываем новый код
+        fl.close()   # закрываем файл
 
 
 minify_js('./js_in.txt', './js_out.txt').main()  # вызов
